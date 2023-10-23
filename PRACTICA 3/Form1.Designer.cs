d399d9a260ceffe3fd28b785dad9b08d794e42b1
tiago <tiago@DESKTOP-00HI8IK>
2023-10-23 09:16:24 -0500

    Agregar archivos de proyecto.

---
+++ PRACTICA 3/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace PRACTICA_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ActiveBorder;
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.Location = new Point(117, 109);
+            label1.Name = "label1";
+            label1.Size = new Size(110, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Primer Numero";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.ActiveBorder;
+            label2.ForeColor = SystemColors.ActiveCaptionText;
+            label2.Location = new Point(117, 206);
+            label2.Name = "label2";
+            label2.Size = new Size(126, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Segundo Numero";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.ActiveBorder;
+            label3.ForeColor = SystemColors.ActiveCaptionText;
+            label3.Location = new Point(186, 315);
+            label3.Name = "label3";
+            label3.Size = new Size(56, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Suma=";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(335, 105);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(201, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(335, 206);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(201, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveBorder;
+            button1.Font = new Font("Arial", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(332, 383);
+            button1.Name = "button1";
+            button1.Size = new Size(192, 45);
+            button1.TabIndex = 5;
+            button1.Text = "SUMAR";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            ForeColor = SystemColors.ActiveBorder;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Sumar Numeros";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+    }
+}
---
+++ PRACTICA 3/Form1.cs
@@ -0,0 +1,23 @@
+namespace PRACTICA_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(textBox1.Text);
+            int num2 = int.Parse(textBox1.Text);
+            int sum = num1 + num2;
+            label3.Text = "Sum = " + sum;
+        }
+    }
+}
